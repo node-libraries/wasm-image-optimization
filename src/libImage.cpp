60147e3cf5fe7ef16bf1c06757e3ed18102ea1c1
SoraKumo <info@croud.jp>
2023-12-28 22:40:42 +0900

    Add new C++ standard library headers and clone external libraries

--- src/libImage.cpp
+++ src/libImage.cpp
@@ -7,75 +7,86 @@
 
 using namespace emscripten;
 
-EM_JS(void, js_console_log, (const char* str), {
-  console.log(UTF8ToString(str));
+EM_JS(void, js_console_log, (const char *str), {
+    console.log(UTF8ToString(str));
 });
 
-struct MemBuffer {
-    std::vector<uint8_t> buffer;
+class MemoryRW
+{
+public:
+    MemoryRW()
+    {
+        m_rw = SDL_AllocRW();
+        m_rw->hidden.unknown.data1 = &m_buffer;
+        m_rw->write = MemWrite;
+        m_rw->close = MemClose;
+    }
+    ~MemoryRW()
+    {
+        SDL_FreeRW(m_rw);
+    }
+    operator SDL_RWops *() const { return m_rw; }
+    size_t size() const { return m_buffer.size(); }
+    const uint8_t *data() const { return m_buffer.data(); }
+
+protected:
+    static size_t MemWrite(SDL_RWops *context, const void *ptr, size_t size, size_t num)
+    {
+        std::vector<uint8_t> *buffer = (std::vector<uint8_t> *)context->hidden.unknown.data1;
+        const uint8_t *bytes = (const uint8_t *)ptr;
+        buffer->insert(buffer->end(), bytes, bytes + size * num);
+        return num;
+    }
+    static int MemClose(SDL_RWops *context)
+    {
+        return 0;
+    }
+
+private:
+    SDL_RWops *m_rw;
+    std::vector<uint8_t> m_buffer;
 };
 
-size_t MemWrite(SDL_RWops* context, const void* ptr, size_t size, size_t num) {
-    MemBuffer* mem = (MemBuffer*)context->hidden.unknown.data1;
-    const uint8_t* bytes = (const uint8_t*)ptr;
-    mem->buffer.insert(mem->buffer.end(), bytes, bytes + size * num);
-    return num;
-}
-int MemClose(SDL_RWops* context) {
-    return 0;
-}
-
-
-SDL_RWops* CreateMemBuffer() {
-    SDL_RWops* rw = SDL_AllocRW();
-    if (rw != nullptr) {
-        rw->hidden.unknown.data1 = new MemBuffer();
-        rw->write = MemWrite;
-        rw->close = MemClose;
-    }
-    return rw;
-}
-
-void FreeMemBuffer(SDL_RWops* rw) {
-    if (rw != nullptr) {
-        delete (MemBuffer*)rw->hidden.unknown.data1;
-        SDL_FreeRW(rw);
-    }
-}
-
-
-val optimize(std::string img_in, float width, float height, float quality,std::string format) {
-    SDL_RWops* rw = SDL_RWFromConstMem(img_in.c_str(), img_in.size());
-    if (!rw) {
+val optimize(std::string img_in, float width, float height, float quality, std::string format)
+{
+    SDL_RWops *rw = SDL_RWFromConstMem(img_in.c_str(), img_in.size());
+    if (!rw)
+    {
         return val::null();
     }
-    
-    SDL_Surface* srcSurface = IMG_Load_RW(rw, 1);
+
+    SDL_Surface *srcSurface = IMG_Load_RW(rw, 1);
     SDL_FreeRW(rw);
-    if (!srcSurface) {
+    if (!srcSurface)
+    {
         return val::null();
     }
-    
+
     int srcWidth = srcSurface->w;
     int srcHeight = srcSurface->h;
-    if (srcWidth == 0 || srcHeight == 0) {
+    if (srcWidth == 0 || srcHeight == 0)
+    {
         SDL_FreeSurface(srcSurface);
         return val::null();
     }
 
-    int outWidth = width?width:srcWidth;
-    int outHeight = height?height:srcHeight;
+    int outWidth = width ? width : srcWidth;
+    int outHeight = height ? height : srcHeight;
     float aspectSrc = static_cast<float>(srcWidth) / srcHeight;
     float aspectDest = outWidth / outHeight;
-    
-    if (aspectSrc > aspectDest) {
+
+    if (aspectSrc > aspectDest)
+    {
         outHeight = outWidth / aspectSrc;
-    } else {
+    }
+    else
+    {
         outWidth = outHeight * aspectSrc;
     }
 
-    SDL_Surface* newSurface = SDL_CreateRGBSurfaceWithFormat(0, static_cast<int>(outWidth), static_cast<int>(outHeight), 32, SDL_PIXELFORMAT_RGBA32);
-    if (!newSurface) {
+    SDL_Surface *newSurface = SDL_CreateRGBSurfaceWithFormat(0, static_cast<int>(outWidth), static_cast<int>(outHeight), 32, SDL_PIXELFORMAT_RGBA32);
+    if (!newSurface)
+    {
         SDL_FreeSurface(srcSurface);
         return val::null();
     }
@@ -83,38 +94,42 @@ val optimize(std::string img_in, float width, float height, float quality,std::s
     SDL_BlitScaled(srcSurface, nullptr, newSurface, nullptr);
     SDL_FreeSurface(srcSurface);
 
-    uint8_t* img_out = nullptr;
-    size_t size = 0;
-    if(format == "png" || format == "jpeg"){
-        SDL_RWops* rw = CreateMemBuffer();
-        if(format == "png"){
-            IMG_SavePNG_RW(newSurface, rw, 1);
-        } else {
-            IMG_SaveJPG_RW(newSurface, rw, 1,quality);
+    if (format == "png" || format == "jpeg")
+    {
+        MemoryRW memoryRW;
+        if (format == "png")
+        {
+            IMG_SavePNG_RW(newSurface, memoryRW, 1);
+        }
+        else
+        {
+            IMG_SaveJPG_RW(newSurface, memoryRW, 1, quality);
         }
         SDL_FreeSurface(newSurface);
-        MemBuffer* mem = (MemBuffer*)rw->hidden.unknown.data1;
-        size = mem->buffer.size();
         val result = val::null();
-        if(size){
-            result = val::global("Uint8Array").new_(typed_memory_view(size,  mem->buffer.data()));
+        if (memoryRW.size())
+        {
+            result = val::global("Uint8Array").new_(typed_memory_view(memoryRW.size(), memoryRW.data()));
         }
-        FreeMemBuffer(rw);
         return result;
-    } else {
+    }
+    else
+    {
+        uint8_t *img_out;
         val result = val::null();
         int stride = static_cast<int>(outWidth) * 4;
-        size = WebPEncodeRGBA(reinterpret_cast<uint8_t*>(newSurface->pixels), static_cast<int>(outWidth), static_cast<int>(outHeight), stride, quality, &img_out);
-        if (size > 0 && img_out) {
+        size_t size = WebPEncodeRGBA(reinterpret_cast<uint8_t *>(newSurface->pixels), static_cast<int>(outWidth), static_cast<int>(outHeight), stride, quality, &img_out);
+        if (size > 0 && img_out)
+        {
             result = val::global("Uint8Array").new_(typed_memory_view(size, img_out));
         }
         WebPFree(img_out);
         SDL_FreeSurface(newSurface);
         return result;
     }
-
 }
 
-EMSCRIPTEN_BINDINGS(my_module) {
-  function("optimize", &optimize);
+EMSCRIPTEN_BINDINGS(my_module)
+{
+    function("optimize", &optimize);
 }
