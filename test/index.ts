eaaba3d25d5e4cf281201d23c793448902303835
SoraKumo <info@croud.jp>
2025-03-02 21:15:08 +0900

    refactor: Update SVG rendering logic for better scalability and readability

--- test/index.ts
+++ test/index.ts
@@ -34,8 +34,12 @@ const main = async () => {
               label,
               `${Math.floor(encoded.length / 1024).toLocaleString()}KB`
             );
-            const fileName = file.split(".")[0];
-            fs.writeFile(`image_output/${fileName}.${format}`, encoded);
+            const fileName = file.split(".");
+            const filePath =
+              format === "none"
+                ? `image_output/${fileName[0]}_.${fileName[1]}`
+                : `image_output/${fileName[0]}.${format}`;
+            fs.writeFile(filePath, encoded);
           }
         });
       });
@@ -43,60 +47,60 @@ const main = async () => {
     });
   });
   await Promise.all(p);
-  // const urls = [
-  //   "https://raw.githubusercontent.com/AOMediaCodec/av1-avif/refs/heads/master/testFiles/Apple/multilayer_examples/animals_00_singlelayer.avif",
-  //   "https://raw.githubusercontent.com/AOMediaCodec/av1-avif/refs/heads/master/testFiles/Microsoft/Chimera_10bit_cropped_to_1920x1008.avif",
-  // ];
-  // for (const url of urls) {
-  //   const data = await fetch(url).then((res) => res.arrayBuffer());
-  //   const u = new URL(url);
-  //   const file = u.pathname.split("/").pop();
-  //   if (!file) return;
-  //   optimizeImage({
-  //     image: data,
-  //     quality: 100,
-  //     format: "jpeg",
-  //     width: 300,
-  //     speed: 6,
-  //   }).then(async () => {
-  //     for (const format of formats) {
-  //       optimizeImage({
-  //         image: data,
-  //         quality: 100,
-  //         format,
-  //         width: 100,
-  //         height: 100,
-  //       }).then((encoded) => {
-  //         console.log(encoded ? true : false, file, format);
-  //         if (encoded) {
-  //           const fileName = file.split(".");
-  //           const filePath =
-  //             format === "none"
-  //               ? `image_output/${fileName[0]}_.${fileName[1]}`
-  //               : `image_output/${fileName[0]}.${format}`;
-  //           fs.writeFile(filePath, encoded);
-  //         }
-  //       });
-  //     }
-  //   });
-  // }
+  const urls = [
+    "https://raw.githubusercontent.com/AOMediaCodec/av1-avif/refs/heads/master/testFiles/Apple/multilayer_examples/animals_00_singlelayer.avif",
+    "https://raw.githubusercontent.com/AOMediaCodec/av1-avif/refs/heads/master/testFiles/Microsoft/Chimera_10bit_cropped_to_1920x1008.avif",
+  ];
+  for (const url of urls) {
+    const data = await fetch(url).then((res) => res.arrayBuffer());
+    const u = new URL(url);
+    const file = u.pathname.split("/").pop();
+    if (!file) return;
+    optimizeImage({
+      image: data,
+      quality: 100,
+      format: "jpeg",
+      width: 300,
+      speed: 6,
+    }).then(async () => {
+      for (const format of formats) {
+        optimizeImage({
+          image: data,
+          quality: 100,
+          format,
+          width: 100,
+          height: 100,
+        }).then((encoded) => {
+          console.log(encoded ? true : false, file, format);
+          if (encoded) {
+            const fileName = file.split(".");
+            const filePath =
+              format === "none"
+                ? `image_output/${fileName[0]}_.${fileName[1]}`
+                : `image_output/${fileName[0]}.${format}`;
+            fs.writeFile(filePath, encoded);
+          }
+        });
+      }
+    });
+  }
 
-  // for (let i = 0; i <= 8; i++) {
-  //   const data = await fetch(
-  //     `https://raw.githubusercontent.com/recurser/exif-orientation-examples/master/Landscape_${i}.jpg`
-  //   ).then((res) => res.arrayBuffer());
-  //   optimizeImage({
-  //     image: data,
-  //     quality: 100,
-  //     format: "jpeg",
-  //     width: 300,
-  //   }).then((encoded) => {
-  //     console.log(encoded ? true : false, `Landscape_${i}.jpg`, "jpeg");
-  //     if (encoded) {
-  //       fs.writeFile(`image_output/Landscape_${i}.jpeg`, encoded);
-  //     }
-  //   });
-  // }
+  for (let i = 0; i <= 8; i++) {
+    const data = await fetch(
+      `https://raw.githubusercontent.com/recurser/exif-orientation-examples/master/Landscape_${i}.jpg`
+    ).then((res) => res.arrayBuffer());
+    optimizeImage({
+      image: data,
+      quality: 100,
+      format: "jpeg",
+      width: 300,
+    }).then((encoded) => {
+      console.log(encoded ? true : false, `Landscape_${i}.jpg`, "jpeg");
+      if (encoded) {
+        fs.writeFile(`image_output/Landscape_${i}.jpeg`, encoded);
+      }
+    });
+  }
   await waitAll();
   close();
 };
